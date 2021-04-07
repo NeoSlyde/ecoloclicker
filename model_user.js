86696fd82b89df67b8514616f598ab37cfaba8ce
Hai Dang <hai-dang.le@etu.univ-amu.fr>
2021-04-07 16:47:29 +0200

    Add code for authentification

---
+++ model_user.js
@@ -0,0 +1,27 @@
+const Sqlite = require('better-sqlite3');
+var bcrypt = require('bcrypt');
+
+let db = new Sqlite('db.sqlite');
+
+
+exports.login = function(name, password){
+    var found = db.prepare('SELECT * FROM user WHERE name = ?').get(name);
+    if(found !== undefined) {
+        console.log(password, found.password);
+      if(bcrypt.compareSync(password, found.password)){
+        return found;
+      }
+      else{
+        return -2;
+      }
+    }
+    else{
+      return -1;
+    }
+  };
+
+exports.new_user = function(name, password){
+    db.prepare("INSERT INTO user (name, password) VALUES (?, ?)").run(name, password);
+    return db.prepare('SELECT * FROM user WHERE name = ?').get(name);
+}
+  
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var mustache = require('mustache-express');
 var bodyParser = require('body-parser');
+var bcrypt = require('bcrypt');
 
 var app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -12,8 +13,16 @@ app.set('view engine', 'html');
 app.set('views', './views');
 
 var plantes = require('./plantes_sql');
+var model_user = require('./model_user');
 
+const cookieSession = require('cookie-session');
+app.use(cookieSession({
+    name: 'session',
+    secret: 'mot-de-passe-du-cookie',
+    maxAge: 24 * 60 * 60 * 1000 // 24 hours
+}));
 
+//============================RENDER=============================//
 app.get('/',(req,res) =>{
     res.render('home');
 })
@@ -22,22 +31,76 @@ app.get('/signup',(req,res) =>{
     res.render('signup');
 });
 
-app.post('/signup',(req,res) =>{
-
-    if(req.body.name != "" && req.body.password != "" && req.body.passwordvalidation == req.body.password){
-        res.redirect('/');
-    }
-    else{
-        res.redirect('/signup');
-    }
-});
-
-app.get("/shop", (req,res) =>{
+app.get("/shop", is_authenticated, (req,res) =>{
     res.render("shop",{plantes_list : plantes.list()});
 });
 
-app.get("/test", (req,res) =>{
-    res.render("test");
+//=========================SESSION===========================//
+
+function is_authenticated(req, res, next) {
+  if (req.session.user !== undefined) {
+    res.locals.name = req.session.user.name;
+    res.locals.authenticated = true;
+    return next();
+  }
+  res.locals.authenticated = false;
+  res.status(401).send('Authentication required');
+}
+
+
+function middleware(req, res, next) {
+  if (req.session.user !== undefined) {
+    res.locals.name = req.session.user.name;
+    res.locals.authenticated = true;
+    return next();
+  } else {
+    res.locals.authenticated = false;
+    return next();
+  }
+}
+
+
+app.post('/signup',(req,res) =>{
+    var hashedPassword = crypt_password(req.body.password);
+    console.log(hashedPassword);
+    const user = model_user.new_user(req.body.name, hashedPassword);
+    console.log(user);
+    req.session.user = user;
+    res.locals.name = req.body.name;
+    res.redirect('/')
 });
+
+function crypt_password(password) {
+    var saved_hash = bcrypt.hashSync(password, 10);
+    return saved_hash;
+}
+
+
+app.post('/login', (req, res) => {
+    const user = model_user.login(req.body.name, req.body.password);
+    console.log(user);
+    if (user != -1 && user != -2) {
+      req.session.user = user;
+      res.locals.name = req.body.name;
+      res.redirect('/shop');
+    } else if (user == -1) {
+      res.locals.failed_name = true;
+      res.redirect('/');
+    } else if (user == -2) {
+      res.locals.failed_pass = true;
+      res.redirect('/');
+    } else {
+      res.redirect('/');
+    }
+  });
+
+
+app.get('/logout', (req, res) => {
+    req.session = null;
+    res.locals.name = null;
+    res.redirect('/');
+});
+
+//========================================================//
 
 app.listen(3000, () => console.log('listening on http://localhost:3000'));
