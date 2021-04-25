34f11a624276a9a79fb4d74ca8a838bd8cba778b
Hai Dang <hai-dang.le@etu.univ-amu.fr>
2021-04-25 15:18:59 +0200

    modification create-db file

--- create-db.js
+++ create-db.js
@@ -17,12 +17,13 @@ var load = function(filename) {
     db.prepare('CREATE TABLE IF NOT EXISTS orders (orderId INTEGER PRIMARY KEY AUTOINCREMENT,userId INT, product TEXT, status TEXT, FOREIGN KEY(userId) REFERENCES users(id) ON DELETE CASCADE)').run();
 	db.prepare('CREATE TABLE IF NOT EXISTS messages (idUser ID, message TEXT)').run();
 	
-   var insert = db.prepare("INSERT INTO Plantes VALUES (@id, @description, @image, @name, 10, 100)");
+   var insert = db.prepare("INSERT INTO Plantes VALUES (@id, @description, @image, @name, 10, @prix)");
 
     var transaction = db.transaction((plantes) =>{
         for(var id = 0;id < plantes.length; id++) {
             var plante = plantes[id];
             plante.id = id;
+            plante.prix = Math.floor(Math.random() * (5000 - 1000) + 1000);
             insert.run(plante);
         }
     });
